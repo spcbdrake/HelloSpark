4c5b70b7641ea28f086fdf371ca6b7bc76fa1e85
benjamindrake <spcbdrake@yahoo.com>
2015-10-26 12:11:31 -0400

    mustache function

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -2,10 +2,30 @@ package com.theironyard;
 
 import spark.Spark;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+        ArrayList<User> users = new ArrayList();
         Spark.staticFileLocation("/public");
         Spark.init();
+        Spark.post(
+                "/create-account",
+                ((request, response) -> {
+                    User user = new User();
+                    user.name = request.queryParams("username");
+                    user.password = request.queryParams("password");
+                    users.add(user);
+                    response.redirect("/");
+                    return "";
+                })
+        );
+        Spark.get(
+                "/accounts",
+                ((request, response) -> {
+                    return "Account page";
+                })
+        );
     }
 }
---
+++ src/com/theironyard/User.java
@@ -0,0 +1,9 @@
+package com.theironyard;
+
+/**
+ * Created by benjamindrake on 10/26/15.
+ */
+public class User {
+    String name;
+    String password;
+}
