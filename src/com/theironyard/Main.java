29e89369256f7cba84b21828eeea3741f9f6112b
benjamindrake <spcbdrake@yahoo.com>
2015-10-27 11:24:22 -0400

    sessions added and login/logout function

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -1,6 +1,7 @@
 package com.theironyard;
 
 import spark.ModelAndView;
+import spark.Session;
 import spark.Spark;
 import spark.template.mustache.MustacheTemplateEngine;
 
@@ -10,29 +11,52 @@ import java.util.HashMap;
 public class Main {
 
     public static void main(String[] args) {
-        ArrayList<User> users = new ArrayList();
-        Spark.staticFileLocation("/public");
+        HashMap<String, User> users = new HashMap();
         Spark.init();
         Spark.post(
                 "/create-account",
                 ((request, response) -> {
-                    User user = new User();
-                    user.name = request.queryParams("username");
-                    user.password = request.queryParams("password");
-                    users.add(user);
+                    String name = request.queryParams("username");
+                    String password = request.queryParams("password");
+
+                    Session session = request.session();
+                    session.attribute("username", name);
+
+                    if (users.get(name) == null) {
+                        User user = new User();
+                        user.name = name;
+                        user.password = password;
+                        users.put(name, user);
+                    }
                     response.redirect("/");
                     return "";
                 })
         );
+
         Spark.get(
-                "/accounts",
+                "/",
                 ((request, response) -> {
+                    Session session = request.session();
+                    String name = session.attribute("username");
+                    if (name != null) {
+                        HashMap m = new HashMap();
+                        m.put("count", users.size());
+                        m.put("accounts", users.values());
+                        return new ModelAndView(m, "logged-in.html");
+                    }
                     HashMap m = new HashMap();
-                    m.put("count", users.size());
-                    m.put("accounts", users);
-                    return new ModelAndView(m, "accounts.html");
+                    return new ModelAndView(m, "not-logged-in.html");
                 }),
                 new MustacheTemplateEngine()
+        );
+        Spark.post(
+                "/logout",
+                ((request, response) -> {
+                    Session session = request.session();
+                    session.invalidate();
+                    response.redirect("/");
+                    return "";
+                })
         );
     }
 }
